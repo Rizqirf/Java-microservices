bf55f978b73f9e9128d22c6ed728c27c629bbfac
Rizqi irfan <r.irfannawwaf@gmail.com>
2023-01-24 23:10:42 +0700

    add order service

--- services/order-service/src/main/java/com/microservices/orderservice/controller/OrderController.java
+++ services/order-service/src/main/java/com/microservices/orderservice/controller/OrderController.java
@@ -1,17 +1,22 @@
 package com.microservices.orderservice.controller;
 
 import com.microservices.orderservice.dto.OrderRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.microservices.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderController {
 
+    private final OrderService orderService;
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        return "Order Placed SSuccessfully";
+        orderService.placeOrder((orderRequest));
+        return "Order Placed Successfully";
     }
 }
